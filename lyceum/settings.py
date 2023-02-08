c03429c9f784971e887616b753274b9c4bc2f06c
Egor <e.v.schukin@yandex.ru>
2023-02-08 08:08:47 +0300

    Issue fixes

--- lyceum/settings.py
+++ lyceum/settings.py
@@ -14,8 +14,19 @@ from pathlib import Path
 import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-env = environ.Env()
-environ.Env.read_env()
+env = environ.Env(
+    DEBUG=(bool, True),
+    SECRET_KEY=(str, 'some-fake-key'),
+    ALLOWED_HOSTS=(list, ['*']),
+    TIME_ZONE=(str, 'UTC'),
+    LANGUAGE_CODE=(str, 'en-us'),
+    STATIC_URL=(str, '/static/'),
+    USE_I18N=(bool, True),
+    USE_L10N=(bool, True),
+    USE_TZ=(bool, True),
+    DB_NAME=(str, 'db.sqlite3')
+)
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -26,7 +37,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 
 # Application definition
