b49368d55e3069aa79abe204b37b94dff86e3177
Egor <61789375+code-n-cry@users.noreply.github.com>
2023-02-11 20:10:23 +0300

    Update urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('', views.item_list),
-               path('<int:number>', views.item_detail)]
+urlpatterns = [path('', views.item_list), path('<int:number>', views.item_detail)]
