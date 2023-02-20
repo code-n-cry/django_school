500a0a0b294a01de379a8348797f1b7d102a9dfb
Egor <e.v.schukin@yandex.ru>
2023-02-20 21:28:19 +0300

    Fix black formatting

--- catalog/fields.py
+++ catalog/fields.py
@@ -6,12 +6,15 @@ import catalog.validators
 
 class ValidatedNameField(django.db.models.TextField):
     def __init__(self, protection_table_cls):
-        verbose_name = 'Название',
-        help_text = 'Добавьте название товара',
-        max_length = 150,
+        verbose_name = ('Название',)
+        help_text = ('Добавьте название товара',)
+        max_length = (150,)
         default_validators = [
             catalog.validators.ValidateSameWriting(protection_table_cls)
         ]
         super().__init__(
-            verbose_name, help_text, max_length, default_validators,
+            verbose_name,
+            help_text,
+            max_length,
+            default_validators,
         )
