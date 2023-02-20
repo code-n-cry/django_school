3833cc218929df4a554ebb82a96744e231bb0296
Egor <e.v.schukin@yandex.ru>
2023-02-20 21:40:09 +0300

    Fix black formatting

--- catalog/migrations/0005_auto_20230220_2130.py
+++ catalog/migrations/0005_auto_20230220_2130.py
@@ -7,7 +7,6 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('catalog', '0004_categoriesuniquenames_tagsuniquenames'),
     ]
@@ -20,26 +19,64 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='category',
             name='slug',
-            field=models.CharField(help_text='Напишите код для url(только лат. буквы, цифры и `-`, `_`)', max_length=200, unique=True, validators=[django.core.validators.RegexValidator('^[0-9a-zA-Z-_]*$', 'Использовать можно только цифры, буквы латиницы и `-`, `_`')], verbose_name='символьный код для url'),
+            field=models.CharField(
+                help_text='Напишите код для url(только лат. буквы, цифры и `-`, `_`)',
+                max_length=200,
+                unique=True,
+                validators=[
+                    django.core.validators.RegexValidator(
+                        '^[0-9a-zA-Z-_]*$',
+                        'Использовать можно только цифры, буквы латиницы и `-`, `_`',
+                    )
+                ],
+                verbose_name='символьный код для url',
+            ),
         ),
         migrations.AlterField(
             model_name='item',
             name='catalog_category',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='items', to='catalog.category', verbose_name='категория'),
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name='items',
+                to='catalog.category',
+                verbose_name='категория',
+            ),
         ),
         migrations.AlterField(
             model_name='item',
             name='tags',
-            field=models.ManyToManyField(related_name='items', to='catalog.Tag', verbose_name='тег'),
+            field=models.ManyToManyField(
+                related_name='items', to='catalog.Tag', verbose_name='тег'
+            ),
         ),
         migrations.AlterField(
             model_name='item',
             name='text',
-            field=models.TextField(help_text='Описание(>2 символов, содержит "превосходно, роскошно")', validators=[django.core.validators.MinLengthValidator(2), catalog.validators.ValidateMustContain('превосходно', 'роскошно')], verbose_name='описание'),
+            field=models.TextField(
+                help_text='Описание(>2 символов, содержит "превосходно, роскошно")',
+                validators=[
+                    django.core.validators.MinLengthValidator(2),
+                    catalog.validators.ValidateMustContain(
+                        'превосходно', 'роскошно'
+                    ),
+                ],
+                verbose_name='описание',
+            ),
         ),
         migrations.AlterField(
             model_name='tag',
             name='slug',
-            field=models.CharField(help_text='Напишите код для url(только лат. буквы, цифры и `-`, `_`)', max_length=200, unique=True, validators=[django.core.validators.RegexValidator('^[0-9a-zA-Z-_]*$', 'Использовать можно только цифры, буквы латиницы и `-`, `_`')], verbose_name='символьный код для url'),
+            field=models.CharField(
+                help_text='Напишите код для url(только лат. буквы, цифры и `-`, `_`)',
+                max_length=200,
+                unique=True,
+                validators=[
+                    django.core.validators.RegexValidator(
+                        '^[0-9a-zA-Z-_]*$',
+                        'Использовать можно только цифры, буквы латиницы и `-`, `_`',
+                    )
+                ],
+                verbose_name='символьный код для url',
+            ),
         ),
     ]
