6d5359751e5ab6a7a98ef5cfa6c8c18f15e9e826
Egor <e.v.schukin@yandex.ru>
2023-03-12 20:41:36 +0300

    Fix formatting

--- about/urls.py
+++ about/urls.py
@@ -5,5 +5,5 @@ from about import views
 app_name = 'about'
 urlpatterns = [
     path('', views.description, name='description'),
-    path('thanks/', views.thanks_for_feedback, name='thanks'),    
+    path('thanks/', views.thanks_for_feedback, name='thanks'),
 ]
